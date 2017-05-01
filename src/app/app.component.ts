bba9f812101d9d1bee925b807b2f3972e701f008
wzh196423 <570251170@qq.com>
2017-05-01 23:23:36 +0800

    first test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {hero} from "./hero";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+    <h1>{{title}}</h1>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="myhero.name" placeholder="aaa">
+    </div>
+    <h2>{{myhero.name}} is my favourite!</h2>
+    <p *ngIf="heroes.length >= 3">There are many Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+      {{hero.id}}   {{hero.name}}
+      </li> 
+    </ul>
+    `,
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes = [new hero(11,'X-man') , new hero(12,'Wolf') , new hero(13 , 'IceBird')];
+  myhero = this.heroes[0];
 }
---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by wangziheng on 2017/4/26.
+ */
+export class hero{
+  constructor (
+    public id : number,
+    public name :string ) { }
+}
